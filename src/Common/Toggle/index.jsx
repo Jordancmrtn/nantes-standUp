94806c0f16c767d1eb8fd2ced8bbc2da5dc01556
Jordan Caumartin <jordan.caumartin@teester.com>
2022-06-22 13:26:18 +0200

    fix(Toggle): clean code

--- src/Common/Toggle/index.jsx
+++ src/Common/Toggle/index.jsx
@@ -3,7 +3,6 @@ import classNames from 'classnames'
 import style from './style.module.css'
 
 const Toggle = ({ checked, onChange }) => {
-    console.log(checked);
   return (
     <label className={style.switch}>
         <input type="checkbox" checked={checked} onChange={e => onChange(e.target.checked)}/>
