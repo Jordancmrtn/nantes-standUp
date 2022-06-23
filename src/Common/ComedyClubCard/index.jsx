f0a5e63cb8a69617b79482b67279dfd85f796245
Jordan Caumartin <jordan.caumartin@teester.com>
2022-06-23 18:44:38 +0200

    fix(comedyClubCard): r&d

--- src/Common/ComedyClubCard/index.jsx
+++ src/Common/ComedyClubCard/index.jsx
@@ -1,17 +1,26 @@
 import PropTypes from 'prop-types';
 import Icon from '../Icon';
+import Test from './assets/test.jpeg'
 
 import style from './style.module.css';
 
-const ComedyClubCard = ({ name, address, instagram, tickets, hour, place }) => {
+const ComedyClubCard = ({ name, address, instagram, tickets, hour, place, info }) => {
   return (
-    <div>
-      {name}
-      {place}
-      {address}
-      {instagram && <a href={instagram} target="_blank" rel="noreferrer"><Icon type="INSTAGRAM" color='white' className={style.icon} /></a>}
-      {tickets && <a href={tickets} target="_blank" rel="noreferrer"><Icon type="TICKET" color='white' className={style.icon}/></a>}
-      {hour}
+    <div className={style.cardContainer}>
+      <img src={Test} alt="test"/>
+      <b>{name}{hour && ` - ${hour}`}</b> <br/>
+      {place} <br/>
+      {address} <br/>
+      {info}  <br/>
+      <div className={style.instagramAndTicketContainer}>
+        {instagram && <a href={instagram} target="_blank" rel="noreferrer"><Icon type="INSTAGRAM" color='white' className={style.icon} /></a>} <br/>
+        {tickets &&
+          <div className={style.ticketsContainer}>
+            <p>Tickets :</p>
+            <a href={tickets} target="_blank" rel="noreferrer"><Icon type="TICKET" color='white' className={style.icon} /></a>
+          </div>
+        }
+      </div>
     </div>
   )
 };
