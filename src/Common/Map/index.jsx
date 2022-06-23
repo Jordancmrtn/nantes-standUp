4cd6e70bed2a3850deee39b2017ca468e02e4e78
Jordan Caumartin <jordan.caumartin@teester.com>
2022-06-23 18:56:18 +0200

    fix(map): add toggle in

--- src/Common/Map/index.jsx
+++ src/Common/Map/index.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
 import PropTypes from 'prop-types'
+import Toggle from '../Toggle'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 
 import style from './style.module.css'
 
-const Map = ({comedyClubToDisplay}) => {
+const Map = ({comedyClubToDisplay, checked, setChecked}) => {
   return (
     <div className={style.mapContainer}>
+      <div className={style.toggleContainer}>
+        <p>Afficher tout les comedy clubs</p>
+        <Toggle checked={checked} onChange={  () => {setChecked(!checked)}}/>
+      </div>
         <MapContainer center={[47.191, -1.55]} minZoom={14} maxZoom={18} zoom={14} scrollWheelZoom={true}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
