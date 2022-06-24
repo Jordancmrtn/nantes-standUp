82f9f072434268f13faeeca4d9ee05c6eba1c089
Jordan Caumartin <jordan.caumartin@teester.com>
2022-06-24 18:05:28 +0200

    feat(img): add all images

---
+++ src/Common/ComedyClubCard/assets/index.js
@@ -0,0 +1,29 @@
+import MicroComedyClub  from './microComedyClub.png'
+import CaboulotComedyClub  from './caboulotComedyClub.png'
+import KissMeComedyClub from './kissMeComedyClub.png'
+import LaPauseStandUp from './laPauseStandUp.png'
+import LeGrenierABlagues from './leGrenierABlagues.png'
+import LundiDeMerde from './lundiDeMerde.png'
+import NouvellesBlagues from './nouvellesBlagues.png'
+import PizzaComedyClub from './pizzaComedyClub.png'
+import PoumComedy from './poumComedy.png'
+import T3ComedyClub from './t3ComedyClub.png'
+import WestSideComedyClub from './westSideComedyClub.png'
+import WhyNotComedyClub from './whyNotComedyClub.png'
+
+const images = {
+    microComedyClub: MicroComedyClub,
+    caboulotComedyClub: CaboulotComedyClub,
+    kissMeComedyClub: KissMeComedyClub,
+    laPauseStandUp: LaPauseStandUp,
+    leGrenierABlagues: LeGrenierABlagues,
+    lundiDeMerde: LundiDeMerde,
+    nouvellesBlagues: NouvellesBlagues,
+    pizzaComedyClub: PizzaComedyClub,
+    poumComedy: PoumComedy,
+    t3ComedyClub: T3ComedyClub,
+    westSideComedyClub: WestSideComedyClub,
+    whyNotComedyClub: WhyNotComedyClub,
+}
+
+export default images
